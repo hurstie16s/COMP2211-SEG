b1504890aaa869e68d186f974437c6913b0efe72
david <davidkuc6@gmail.com>
2023-02-23 03:19:01 +0000

    Initial setup of environment - Add JavaFX

--- src/main/java/comp2211/seg/App.java
+++ src/main/java/comp2211/seg/App.java
@@ -1,10 +1,22 @@
 package comp2211.seg;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-
-public class App {
+import javafx.application.Application;
+import javafx.stage.Stage;
+public class App extends Application{
+    private static App instance;
+    private Stage stage;
     private static final Logger logger = LogManager.getLogger(App.class);
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        logger.info("Starting");
+        launch();
+    }
+
+    @Override
+    public void start(Stage stage) {
+
+        instance = this;
+        this.stage = stage;
+        //Do stuff
     }
 }
---
+++ src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module runwaytool {
+    requires java.scripting;
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires org.apache.logging.log4j;
+    exports comp2211.seg;
+}
