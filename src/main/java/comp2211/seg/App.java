41ed7caf2aa798b6a4041624db21555607ff256e
aleks <ajp1e19@sonton.ac.uk>
2023-02-24 18:22:53 +0000

    Added: to pom: javafx plugin - so it runs mvn javafx:run command;to main dir :resources dir and in there log4j2 xml file - now it prints in console;to comp2211.seg dir: MVC packages; to UiView package: HomeView class; to App.class code to display and shutdown HomeView object.

--- src/main/java/comp2211/seg/App.java
+++ src/main/java/comp2211/seg/App.java
@@ -1,24 +1,51 @@
 package comp2211.seg;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+
+import comp2211.seg.UiView.HomeView;
 import javafx.application.Application;
 import javafx.stage.Stage;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
-public class App extends Application{
-    private static App instance;
-    private Stage stage;
-    private static final Logger logger = LogManager.getLogger(App.class);
-    public static void main(String[] args) {
-        logger.info("Starting");
-        launch();
-    }
+public class App extends Application {
 
-    @Override
-    public void start(Stage stage) {
+  private static App instance;
+  private Stage stage;
+  private static final Logger logger = LogManager.getLogger(App.class);
 
-        logger.info("say hi");
-        instance = this;
-        this.stage = stage;
-        //Do stuff
-    }
+  public static void main(String[] args) {
+    logger.info("Starting");
+    launch();
+
+  }
+
+  @Override
+  public void start(Stage stage) {
+
+    var javaVersion = SystemInfo.javaVersion();
+    var javafxVersion = SystemInfo.javafxVersion();
+    logger.info("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
+
+    instance = this;
+    this.stage = stage;
+
+    displayHomeView();
+  }
+
+  public void displayHomeView() {
+    logger.info("Opening HomeView");
+    //int width = 200;
+    //int height = 200;
+    new HomeView(stage); //, width, height);
+    stage.show();
+
+  }
+
+  public void shutdown() {
+    logger.info("Shutting down");
+    System.exit(0);
+  }
+
+  public static App getInstance() {
+    return instance;
+  }
 }
