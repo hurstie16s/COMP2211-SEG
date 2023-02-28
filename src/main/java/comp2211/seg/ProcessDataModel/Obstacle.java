e7cd0a8660df90f95294e9e4f08bb76a43675a7a
hurstie16s <samuel.hurst2@btinternet.com>
2023-02-28 16:01:29 +0000

    added add and remove obstacle methods, need to look into value re-calculation after that made base structure for object class for possibly needed inputs

--- src/main/java/comp2211/seg/ProcessDataModel/Obstacle.java
+++ src/main/java/comp2211/seg/ProcessDataModel/Obstacle.java
@@ -1,4 +1,18 @@
 package comp2211.seg.ProcessDataModel;
 
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.SimpleStringProperty;
+
 public class Obstacle {
+
+    private final SimpleStringProperty obstacleDesignator = new SimpleStringProperty();
+    private final SimpleDoubleProperty height = new SimpleDoubleProperty();
+    private final SimpleDoubleProperty distFromThreshold = new SimpleDoubleProperty();
+
+    public Obstacle(String obstacleDesignator, double height, double distFromThreshold) {
+        this.obstacleDesignator.set(obstacleDesignator);
+        this.height.set(height);
+        this.distFromThreshold.set(distFromThreshold);
+    }
+
 }
--- src/main/java/comp2211/seg/ProcessDataModel/Runway.java
+++ src/main/java/comp2211/seg/ProcessDataModel/Runway.java
@@ -3,41 +3,59 @@ package comp2211.seg.ProcessDataModel;
 import javafx.beans.property.SimpleDoubleProperty;
 import javafx.beans.property.SimpleStringProperty;
 
+import java.util.ArrayList;
+
 public class Runway {
     //Inputs
+
     /*
     A runway designator consists of a two-digit number,
     which is the whole number nearest to one tenth of the magnetic North
     when viewed from the direction of approach. For example,
     if the azimuth of the centre-line is 153 then the runway designator will be 15
      */
-    private final SimpleStringProperty RUNWAYDESIGNATOR = new SimpleStringProperty();
+    private final SimpleStringProperty runwayDesignator = new SimpleStringProperty();
     private final SimpleDoubleProperty tora = new SimpleDoubleProperty();
     private final SimpleDoubleProperty toda = new SimpleDoubleProperty();
     private final SimpleDoubleProperty asda = new SimpleDoubleProperty();
     private final SimpleDoubleProperty lda = new SimpleDoubleProperty();
     private final SimpleDoubleProperty dispThreshold = new SimpleDoubleProperty();
 
+    private final ArrayList<Obstacle> runwayObstacles = new ArrayList<>();
+
+    // End of Inputs
 
     // Typical values, may become variable down the line
-    private final SimpleDoubleProperty MINRESA = new SimpleDoubleProperty(240);
-    private final SimpleDoubleProperty ALSMAGNITUDE = new SimpleDoubleProperty(50);
-    private final SimpleDoubleProperty STRIPEND = new SimpleDoubleProperty(60);
-    private final SimpleDoubleProperty BLASTZONE = new SimpleDoubleProperty(500);
+    // Constants
+    private static final SimpleDoubleProperty MINRESA = new SimpleDoubleProperty(240);
+    private static final SimpleDoubleProperty ALSMAGNITUDE = new SimpleDoubleProperty(50);
+    private static final SimpleDoubleProperty STRIPEND = new SimpleDoubleProperty(60);
+    private static final SimpleDoubleProperty BLASTZONE = new SimpleDoubleProperty(500);
 
     //Outputs
     private SimpleDoubleProperty output1 = new SimpleDoubleProperty(0);
     private SimpleDoubleProperty output2 = new SimpleDoubleProperty(0);
     private SimpleDoubleProperty output3 = new SimpleDoubleProperty(0);
 
-    public Runway(String RUNWAYDESIGNATOR, double tora, double toda, double asda, double lda, double dispThreshold) {
-        this.RUNWAYDESIGNATOR.set(RUNWAYDESIGNATOR);
+    public Runway(String runwayDesignator, double tora, double toda, double asda, double lda, double dispThreshold) {
+        this.runwayDesignator.set(runwayDesignator);
         this.tora.set(tora);
         this.toda.set(toda);
         this.asda.set(asda);
         this.lda.set(lda);
         this.dispThreshold.set(dispThreshold);
     }
+
+    public void addObstacle(Obstacle obstacleToAdd) {
+        runwayObstacles.add(obstacleToAdd);
+        // re-calculate values?
+    }
+
+    public void removeObstacle(Obstacle obstacleToRemove) {
+        runwayObstacles.remove(obstacleToRemove);
+        // re-calculate values?
+    }
+
     public void calculateLandOver() {}
 
     public void calculateLandTowards() {}
@@ -45,4 +63,7 @@ public class Runway {
     public void calculateTakeOffToward() {}
 
     public void calculateTakeOffAway() {}
+
+    // Setters
+    // Getters
 }
