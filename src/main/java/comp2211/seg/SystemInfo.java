9d3f26c29fa59f40d430b11844e15e8de887b7ab
aleks <ajp1e19@sonton.ac.uk>
2023-03-02 13:42:18 +0000

    SystemInfo class commented

--- src/main/java/comp2211/seg/SystemInfo.java
+++ src/main/java/comp2211/seg/SystemInfo.java
@@ -1,11 +1,23 @@
 package comp2211.seg;
 
-public class SystemInfo {
+/**
 
+ Utility class to retrieve system information
+ */
+public class SystemInfo {
+  /**
+
+   Retrieves the version of the current Java runtime environment
+   @return String representation of the Java version
+   */
   public static String javaVersion() {
     return System.getProperty("java.version");
   }
+  /**
 
+   Retrieves the version of the current JavaFX runtime environment
+   @return String representation of the JavaFX version
+   */
   public static String javafxVersion() {
     return System.getProperty("javafx.version");
   }
