4ea232c68ddffbccc55afd73c124d78fe71bf48a
aleks <ajp1e19@sonton.ac.uk>
2023-03-04 22:31:25 +0000

    Launcher class commented, plus som edits

--- src/main/java/comp2211/seg/Launcher.java
+++ src/main/java/comp2211/seg/Launcher.java
@@ -1,7 +1,16 @@
 package comp2211.seg;
 
+/**
+ * The Launcher class provides a main method to launch the application.
+ */
 public class Launcher {
+
+    /**
+     * The main method of the Launcher class.
+     * @param args the command-line arguments
+     */
     public static void main(String[] args) {
         App.main(args);
     }
 }
+
--- src/main/java/comp2211/seg/SystemInfo.java
+++ src/main/java/comp2211/seg/SystemInfo.java
@@ -1,22 +1,19 @@
 package comp2211.seg;
 
 /**
-
- Utility class to retrieve system information
+ * Utility class to retrieve system information
  */
 public class SystemInfo {
   /**
-
-   Retrieves the version of the current Java runtime environment
-   @return String representation of the Java version
+   * Retrieves the version of the current Java runtime environment
+   * @return String representation of the Java version
    */
   public static String javaVersion() {
     return System.getProperty("java.version");
   }
   /**
-
-   Retrieves the version of the current JavaFX runtime environment
-   @return String representation of the JavaFX version
+   * Retrieves the version of the current JavaFX runtime environment
+   * @return String representation of the JavaFX version
    */
   public static String javafxVersion() {
     return System.getProperty("javafx.version");
