a135aba312ce7916f69386f88b24acb30a2d1fa9
aleks <ajp1e19@sonton.ac.uk>
2023-03-04 18:39:34 +0000

    Airport class commented

--- src/main/java/comp2211/seg/ProcessDataModel/Airport.java
+++ src/main/java/comp2211/seg/ProcessDataModel/Airport.java
@@ -2,19 +2,40 @@ package comp2211.seg.ProcessDataModel;
 
 import java.util.ArrayList;
 
+/**
+ * An Airport object representing an airport with runways.
+ */
 public class Airport {
+    /** The list of runways at the airport. */
     private ArrayList<Runway> runways;
+    /** The name of the airport. */
     public String name;
-    public Airport(String name){
+
+    /**
+     * Creates a new Airport object with the specified name and a default runway.
+     *
+     * @param name the name of the airport
+     */
+    public Airport(String name) {
         runways = new ArrayList<>();
         this.name = name;
         makeRunway();
     }
-    public void makeRunway(){
+
+    /**
+     * Creates a new runway and adds it to the list of runways at the airport.
+     */
+    public void makeRunway() {
         runways.add(new Runway());
     }
 
+    /**
+     * Returns the list of runways at the airport.
+     *
+     * @return the list of runways at the airport
+     */
     public ArrayList<Runway> getRunways() {
         return runways;
     }
+
 }
