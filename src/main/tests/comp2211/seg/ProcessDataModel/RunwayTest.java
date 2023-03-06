fdb981dcc4c2e2013564ffb1b34769d80b95a715
hurstie16s <samuel.hurst2@btinternet.com>
2023-03-06 12:31:45 +0000

    started looking a writin some dev unit tests for landing calculations

--- src/main/tests/comp2211/seg/ProcessDataModel/RunwayTest.java
+++ src/main/tests/comp2211/seg/ProcessDataModel/RunwayTest.java
@@ -1,42 +1,91 @@
 package comp2211.seg.ProcessDataModel;
 
-import javafx.beans.property.SimpleDoubleProperty;
-import javafx.beans.property.SimpleStringProperty;
+import junitparams.Parameters;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class RunwayTest {
 
-    @Test
-    void recalculate() {
+    static Runway runway1 = new Runway();
+    static Runway runway2 = new Runway();
+    static Runway runway3 = new Runway();
+    static Runway runway4 = new Runway();
+
+    @BeforeAll
+    static void setUpRunways() {
+        setProperties(
+                "09L",
+                3902,
+                3902,
+                3902,
+                3595,
+                306,
+                runway1
+        );
+        setProperties(
+                "27R",
+                3884,
+                3962,
+                3884,
+                3884,
+                0,
+                runway2
+        );
+        setProperties(
+                "09R",
+                3660,
+                3660,
+                3660,
+                3353,
+                307,
+                runway3
+        );
+        setProperties(
+                "27L",
+                3360,
+                3360,
+                3360,
+                3360,
+                0,
+                runway4
+        );
+    }
+
+    static void setProperties(
+            String designator,
+            int tora,
+            int toda,
+            int asda,
+            int lda,
+            int dispThreshold,
+            Runway runway) {
+        runway.runwayDesignatorProperty().set(designator);
+        runway.toraProperty().set(tora);
+        runway.todaProperty().set(toda);
+        runway.asdaProperty().set(asda);
+        runway.ldaProperty().set(lda);
+        runway.dispThresholdProperty().set(dispThreshold);
     }
 
     @Test
-    void calculateLandOver() {
-    }
-
-    Runway[] generateTestData() {
-        var runway1 = new Runway();
-        var runway2 = new Runway();
-        var runway3 = new Runway();
-        var runway4 = new Runway();
-
-        //Set Properties
-
-        return null;
+    void recalculateTest() {
     }
 
     @Test
-    void calculateLandTowards() {
+    @Parameters({"runway1", "runway2", "runway3", "runway4"})
+    void calculateLandOverTest(Runway runway) {
+
     }
 
     @Test
-    void calculateTakeOffToward() {
+    void calculateLandTowardsTest() {
     }
 
     @Test
-    void calculateTakeOffAway() {
+    void calculateTakeOffTowardTest() {
+    }
+
+    @Test
+    void calculateTakeOffAwayTest() {
     }
 }
