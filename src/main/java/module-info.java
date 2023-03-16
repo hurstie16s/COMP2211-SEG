611f05d0d9699d772dda37877a8ade8c79b611e4
david <davidkuc6@gmail.com>
2023-03-16 16:53:59 +0000

    fixes

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,5 @@ module runwaytool {
     requires javafx.controls;
     requires javafx.fxml;
     requires org.apache.logging.log4j;
-    requires org.seleniumhq.selenium.firefox_driver;
     exports comp2211.seg;
 }
