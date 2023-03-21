e18d7cf28e6b55ff1973d21e7fc4a90b3c6952a4
JoshWillson <joshuawillson1@outlook.com>
2023-03-21 17:16:11 +0000

    Length, Height swapped

--- src/main/java/comp2211/seg/UiView/Scene/BaseScene.java
+++ src/main/java/comp2211/seg/UiView/Scene/BaseScene.java
@@ -604,7 +604,7 @@ public class BaseScene extends SceneAbstract implements GlobalVariables{
         rc.add(rcx);
         obstacleData.getRowConstraints().addAll(rc);
 
-        obstacleData.addColumn(0,makeLabel("Preset"),makeLabel("Height"),makeLabel("Length"),makeLabel("Width"),makeLabel("Currently Active?"),makeLabel("Position"));
+        obstacleData.addColumn(0,makeLabel("Preset"),makeLabel("Length"),makeLabel("Height"),makeLabel("Width"),makeLabel("Currently Active?"),makeLabel("Position"));
 
         // Obstacle preset dropdown selector
         obstacleData.add(obstacleComboBox,1,0);
