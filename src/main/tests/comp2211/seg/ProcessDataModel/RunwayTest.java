d9ca81d363a369939480f3e0f1dd8845b322b084
hurstie16s <samuel.hurst2@btinternet.com>
2023-03-21 11:19:33 +0000

    corrected cause for null pointer

--- src/main/tests/comp2211/seg/ProcessDataModel/RunwayTest.java
+++ src/main/tests/comp2211/seg/ProcessDataModel/RunwayTest.java
@@ -3,10 +3,9 @@ package comp2211.seg.ProcessDataModel;
 import javafx.beans.property.SimpleDoubleProperty;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.junit.Test;
+
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
