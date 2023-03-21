23369c96f610252fac66de203f4e5e24fd489fd8
aleks <ajp1e19@sonton.ac.uk>
2023-03-21 09:25:57 +0000

    add inline comment to avoid confusion

--- src/main/java/comp2211/seg/Controller/Interfaces/UKAirports.java
+++ src/main/java/comp2211/seg/Controller/Interfaces/UKAirports.java
@@ -1,28 +1,68 @@
 package comp2211.seg.Controller.Interfaces;
 
-public interface UKAirportsRunways {
+public interface UKAirports {
+  // London Heathrow Airport
   String LHR_RUNWAY_09L_27R = "09L/27R";
   String LHR_RUNWAY_09R_27L = "09R/27L";
+
+  // London Gatwick Airport
   String LGW_RUNWAY_08L_26R = "08L/26R";
   String LGW_RUNWAY_08R_26L = "08R/26L";
+
+  // Manchester Airport
   String MAN_RUNWAY_05L_23R = "05L/23R";
   String MAN_RUNWAY_05R_23L = "05R/23L";
+
+  // London Stansted Airport
   String STN_RUNWAY_04_22 = "04/22";
+
+  // London Luton Airport
   String LTN_RUNWAY_08_26 = "08/26";
+
+  // Edinburgh Airport
   String EDI_RUNWAY_06_24 = "06/24";
+
+  // Birmingham Airport
   String BHX_RUNWAY_15_33 = "15/33";
+
+  // Glasgow Airport
   String GLA_RUNWAY_05_23 = "05/23";
+
+  // Bristol Airport
   String BRS_RUNWAY_09_27 = "09/27";
+
+  // Newcastle Airport
   String NCL_RUNWAY_07_25 = "07/25";
+
+  // East Midlands Airport
   String EMA_RUNWAY_09_27 = "09/27";
+
+  // Liverpool John Lennon Airport
   String LPL_RUNWAY_09_27 = "09/27";
+
+  // London City Airport
   String LCY_RUNWAY_09_27 = "09/27";
+
+  // Leeds Bradford Airport
   String LBA_RUNWAY_14_32 = "14/32";
+
+  // Belfast International Airport
   String BFS_RUNWAY_07_25 = "07/25";
+
+  // Aberdeen International Airport
   String ABZ_RUNWAY_16_34 = "16/34";
+
+  // Southampton Airport
   String SOU_RUNWAY_02_20 = "02/20";
+
+  // Cardiff Airport
   String CWL_RUNWAY_12_30 = "12/30";
+
+  // Glasgow Prestwick Airport
   String PIK_RUNWAY_03_21 = "03/21";
+
+  // London Southend Airport
   String SEN_RUNWAY_06_24 = "06/24";
 }
+
 
