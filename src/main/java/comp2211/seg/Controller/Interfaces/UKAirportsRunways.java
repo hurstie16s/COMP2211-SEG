994fbc9db08d1ca62efc6fe8e4e6f047acd7d44c
aleks <ajp1e19@sonton.ac.uk>
2023-03-21 09:19:45 +0000

    add UKAirportsRunways (20 of them) interface

---
+++ src/main/java/comp2211/seg/Controller/Interfaces/UKAirportsRunways.java
@@ -0,0 +1,28 @@
+package comp2211.seg.Controller.Interfaces;
+
+public interface UKAirportsRunways {
+  String LHR_RUNWAY_09L_27R = "09L/27R";
+  String LHR_RUNWAY_09R_27L = "09R/27L";
+  String LGW_RUNWAY_08L_26R = "08L/26R";
+  String LGW_RUNWAY_08R_26L = "08R/26L";
+  String MAN_RUNWAY_05L_23R = "05L/23R";
+  String MAN_RUNWAY_05R_23L = "05R/23L";
+  String STN_RUNWAY_04_22 = "04/22";
+  String LTN_RUNWAY_08_26 = "08/26";
+  String EDI_RUNWAY_06_24 = "06/24";
+  String BHX_RUNWAY_15_33 = "15/33";
+  String GLA_RUNWAY_05_23 = "05/23";
+  String BRS_RUNWAY_09_27 = "09/27";
+  String NCL_RUNWAY_07_25 = "07/25";
+  String EMA_RUNWAY_09_27 = "09/27";
+  String LPL_RUNWAY_09_27 = "09/27";
+  String LCY_RUNWAY_09_27 = "09/27";
+  String LBA_RUNWAY_14_32 = "14/32";
+  String BFS_RUNWAY_07_25 = "07/25";
+  String ABZ_RUNWAY_16_34 = "16/34";
+  String SOU_RUNWAY_02_20 = "02/20";
+  String CWL_RUNWAY_12_30 = "12/30";
+  String PIK_RUNWAY_03_21 = "03/21";
+  String SEN_RUNWAY_06_24 = "06/24";
+}
+
