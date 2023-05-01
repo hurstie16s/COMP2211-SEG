627314a8f89e8487dd45a376456802f94d26b45e
hurstie16s <samuel.hurst2@btinternet.com>
2023-05-01 16:59:00 +0100

    started testing, looking into 19 again

--- src/test/java/comp2211/seg/ProcessDataModel/RunwayTest.java
+++ src/test/java/comp2211/seg/ProcessDataModel/RunwayTest.java
@@ -1,5 +1,6 @@
 package comp2211.seg.ProcessDataModel;
 
+
 import javafx.beans.property.SimpleDoubleProperty;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -22,145 +23,11 @@ public class RunwayTest {
     //TODO: Test Rew-calculate
     //TODO: Re-work tests to handle left and right
 
-    /**
-     * The constant runway1.
-     */
-// 09R and 27L
-    static Runway runway1 = new Runway();
-    /**
-     * The constant runway2.
-     */
-// 09L and 27R
-    static Runway runway2 = new Runway();
-    /**
-     * The constant runway3.
-     */
-// 07R and 25L
-    static Runway runway3 = new Runway();
-    /**
-     * The constant runway4.
-     */
-// 16L and 34R
-    static Runway runway4 = new Runway();
-    /**
-     * The constant runway5.
-     */
-// 04C and 22C
-    static Runway runway5 = new Runway();
 
-    /**
-     * Sets up runways.
-     */
+
     @BeforeAll
-    public static void setUpRunways() {
-        setProperties(
-                "09R",
-                3660,
-                3660,
-                3660,
-                3353,
-                3660,
-                3660,
-                3660,
-                3660,
-                runway1
-        );
-        setProperties(
-                "09L",
-                3902,
-                3902,
-                3902,
-                3595,
-                3884,
-                3962,
-                3884,
-                3884,
-                runway2
-        );
-        setProperties(
-                "07R",
-                2162,
-                3243,
-                2162,
-                2162,
-                2162,
-                3243,
-                2219,
-                2080,
-                runway3
-        );
-        setProperties(
-                "16L",
-                1953,
-                2152,
-                1953,
-                1953,
-                1953,
-                2091,
-                1953,
-                1953,
-                runway4
-        );
-        setProperties(
-                "04C",
-                968,
-                968,
-                968,
-                968,
-                968,
-                968,
-                968,
-                968,
-                runway5
-        );
-    }
-
-    /**
-     * Sets properties.
-     *
-     * @param designator the designator
-     * @param leftTora   the left tora
-     * @param leftToda   the left toda
-     * @param leftAsda   the left asda
-     * @param leftLda    the left lda
-     * @param rightTora  the right tora
-     * @param rightToda  the right toda
-     * @param rightAsda  the right asda
-     * @param rightLda   the right lda
-     * @param runway     the runway
-     */
-    public static void setProperties(
-            String designator,
-            int leftTora,
-            int leftToda,
-            int leftAsda,
-            int leftLda,
-            int rightTora,
-            int rightToda,
-            int rightAsda,
-            int rightLda,
-            Runway runway) {
-        runway.runwayDesignatorLeftProperty().set(designator);
-        runway.inputLeftToraProperty().bind(new SimpleDoubleProperty(leftTora));
-        runway.inputLeftTodaProperty().bind(new SimpleDoubleProperty(leftToda));
-        runway.inputLeftAsdaProperty().bind(new SimpleDoubleProperty(leftAsda));
-        runway.inputLeftLdaProperty().bind(new SimpleDoubleProperty(leftLda));
-        runway.inputRightToraProperty().bind(new SimpleDoubleProperty(rightTora));
-        runway.inputRightTodaProperty().bind(new SimpleDoubleProperty(rightToda));
-        runway.inputRightAsdaProperty().bind(new SimpleDoubleProperty(rightAsda));
-        runway.inputRightLdaProperty().bind(new SimpleDoubleProperty(rightLda));
-    }
-
-    /**
-     * Add obstacle.
-     *
-     * @param runway   the runway
-     * @param obstacle the obstacle
-     */
-    public void addObstacle(Runway runway, Obstacle obstacle){
-        runway.removeObstacle();
-        obstacle.lengthProperty().set(0);
-        runway.addObstacle(obstacle);
+    public static void setUp() {
+        RunwayTestData.setUpRunways();
     }
 
     // Unit Tests
@@ -282,44 +149,91 @@ public class RunwayTest {
     @DisplayName("Runway Recalculation test: Runway 2 (09L27R), Obstacle 1")
     @Test
     public void runwayRecalculationTestR2O1() {
+        var runway = RunwayTestData.runway2;
+        var obstacle = RunwayTestData.obstacle1;
 
+        runway.addObstacle(obstacle);
+        runway.runwayObstacle.distFromThresholdProperty().set(-50);
+        // Land towards
+        RunwayCalculations.calculateLandTowardLeft(runway);
+        RunwayCalculations.calculateLandTowardRight(runway);
+        // Takeoff towards
+        RunwayCalculations.calculateTakeOffTowardLeft(runway);
+        RunwayCalculations.calculateTakeOffTowardRight(runway);
+
+        assertValues(
+                runway.getLeftLda(),
+                runway.getRightLda(),
+                runway.getLeftTora(),
+                runway.getRightTora(),
+                runway.getLeftToda(),
+                runway.getRightToda(),
+                runway.getLeftAsda(),
+                runway.getRightAsda(),
+                0,
+                3345,
+                0,
+                2985,
+                0,
+                2985,
+                0,
+                2985
+
+        );
+
+        // Land Over
+        RunwayCalculations.calculateLandOverLeft(runway);
+        RunwayCalculations.calculateLandOverRight(runway);
+        // Takeoff away
+        RunwayCalculations.calculateTakeOffAwayLeft(runway);
+        RunwayCalculations.calculateTakeOffAwayRight(runway);
+
+        assertValues(
+                runway.getLeftLda(),
+                runway.getRightLda(),
+                runway.getLeftTora(),
+                runway.getRightTora(),
+                runway.getLeftToda(),
+                runway.getRightToda(),
+                runway.getLeftAsda(),
+                runway.getRightAsda(),
+                2985,
+                0,
+                3095,
+                0,
+                3095,
+                78,
+                3095,
+                0
+
+        );
     }
 
-    /**
-     * The constant obstacle1.
-     */
-// Obstacles defined in given scenario's
-    static Obstacle obstacle1 = new Obstacle("ob1", 12, -50);
-    /**
-     * The Obstacle 2.
-     */
-    static Obstacle obstacle2 = new Obstacle("ob2", 25, 2853);
-    /**
-     * The Obstacle 3.
-     */
-    static Obstacle obstacle3 = new Obstacle("ob3", 15, 150);
-    /**
-     * The Obstacle 4.
-     */
-    static Obstacle obstacle4 = new Obstacle("ob4", 20, 3546);
-    /**
-     * The Obstacle 5.
-     */
-    static Obstacle obstacle5 = new Obstacle("ob5", 14, 80);
-    /**
-     * The Obstacle 6.
-     */
-    static Obstacle obstacle6 = new Obstacle("ob6", 11, 2285);
-    /**
-     * The Obstacle 7.
-     */
-    static Obstacle obstacle7 = new Obstacle("ob7", 13, 976);
-    /**
-     * The Obstacle 8.
-     */
-    static Obstacle obstacle8 = new Obstacle("ob8", 17, 0);
-    /**
-     * The Obstacle 9.
-     */
-    static Obstacle obstacle9 = new Obstacle("ob9", 8, 484);
+    private void assertValues(
+            double leftLDA_actual,
+            double rightLDA_actual,
+            double leftTORA_actual,
+            double rightTORA_actual,
+            double leftTODA_actual,
+            double rightTODA_actual,
+            double leftASDA_actual,
+            double rightASDA_actual,
+            double leftLDA_expected,
+            double rightLDA_expected,
+            double leftTORA_expected,
+            double rightTORA_expected,
+            double leftTODA_expected,
+            double rightTODA_expected,
+            double leftASDA_expected,
+            double rightASDA_expected
+    ) {
+        assertEquals(leftLDA_expected, leftLDA_actual, "Left LDA Incorrect");
+        assertEquals(rightLDA_expected , rightLDA_actual, "Right LDA Incorrect");
+        assertEquals(leftTORA_expected, leftTORA_actual, "Left TORA Incorrect");
+        assertEquals(rightTORA_expected, rightTORA_actual, "Right TORA Incorrect");
+        assertEquals(leftTODA_expected, leftTODA_actual, "Left TODA Incorrect");
+        assertEquals(rightTODA_expected, rightTODA_actual, "Right TODA Incorrect");
+        assertEquals(leftASDA_expected, leftASDA_actual, "Left ASDA Incorrect");
+        assertEquals(rightASDA_expected, rightASDA_actual, "Right ASDA Incorrect");
+    }
 }
---
+++ src/test/java/comp2211/seg/ProcessDataModel/RunwayTestData.java
@@ -0,0 +1,171 @@
+package comp2211.seg.ProcessDataModel;
+
+import javafx.beans.property.SimpleDoubleProperty;
+import org.junit.jupiter.api.BeforeAll;
+
+public class RunwayTestData {
+
+    static Runway runway1 = new Runway();
+    /**
+     * The constant runway2.
+     */
+// 09L and 27R
+    static Runway runway2 = new Runway();
+    /**
+     * The constant runway3.
+     */
+// 07R and 25L
+    static Runway runway3 = new Runway();
+    /**
+     * The constant runway4.
+     */
+// 16L and 34R
+    static Runway runway4 = new Runway();
+    /**
+     * The constant runway5.
+     */
+// 04C and 22C
+    static Runway runway5 = new Runway();
+
+    /**
+     * Sets up runways.
+     */
+    public static void setUpRunways() {
+        setProperties(
+                "09R",
+                3660,
+                3660,
+                3660,
+                3353,
+                3660,
+                3660,
+                3660,
+                3660,
+                runway1
+        );
+        setProperties(
+                "09L",
+                3902,
+                3902,
+                3902,
+                3595,
+                3884,
+                3962,
+                3884,
+                3884,
+                runway2
+        );
+        setProperties(
+                "07R",
+                2162,
+                3243,
+                2162,
+                2162,
+                2162,
+                3243,
+                2219,
+                2080,
+                runway3
+        );
+        setProperties(
+                "16L",
+                1953,
+                2152,
+                1953,
+                1953,
+                1953,
+                2091,
+                1953,
+                1953,
+                runway4
+        );
+        setProperties(
+                "04C",
+                968,
+                968,
+                968,
+                968,
+                968,
+                968,
+                968,
+                968,
+                runway5
+        );
+    }
+
+    /**
+     * Sets properties.
+     *
+     * @param designator the designator
+     * @param leftTora   the left tora
+     * @param leftToda   the left toda
+     * @param leftAsda   the left asda
+     * @param leftLda    the left lda
+     * @param rightTora  the right tora
+     * @param rightToda  the right toda
+     * @param rightAsda  the right asda
+     * @param rightLda   the right lda
+     * @param runway     the runway
+     */
+    public static void setProperties(
+            String designator,
+            int leftTora,
+            int leftToda,
+            int leftAsda,
+            int leftLda,
+            int rightTora,
+            int rightToda,
+            int rightAsda,
+            int rightLda,
+            Runway runway) {
+        runway.runwayDesignatorLeftProperty().set(designator);
+        runway.inputLeftToraProperty().bind(new SimpleDoubleProperty(leftTora));
+        runway.inputLeftTodaProperty().bind(new SimpleDoubleProperty(leftToda));
+        runway.inputLeftAsdaProperty().bind(new SimpleDoubleProperty(leftAsda));
+        runway.inputLeftLdaProperty().bind(new SimpleDoubleProperty(leftLda));
+        runway.inputRightToraProperty().bind(new SimpleDoubleProperty(rightTora));
+        runway.inputRightTodaProperty().bind(new SimpleDoubleProperty(rightToda));
+        runway.inputRightAsdaProperty().bind(new SimpleDoubleProperty(rightAsda));
+        runway.inputRightLdaProperty().bind(new SimpleDoubleProperty(rightLda));
+    }
+
+
+    /**
+     * The constant obstacle1.
+     */
+// Obstacles defined in given scenario's
+    static Obstacle obstacle1 = new Obstacle("ob1", 12, -50);
+    /**
+     * The Obstacle 2.
+     */
+    static Obstacle obstacle2 = new Obstacle("ob2", 25, 2853);
+    /**
+     * The Obstacle 3.
+     */
+    static Obstacle obstacle3 = new Obstacle("ob3", 15, 150);
+    /**
+     * The Obstacle 4.
+     */
+    static Obstacle obstacle4 = new Obstacle("ob4", 20, 3546);
+    /**
+     * The Obstacle 5.
+     */
+    static Obstacle obstacle5 = new Obstacle("ob5", 14, 80);
+    /**
+     * The Obstacle 6.
+     */
+    static Obstacle obstacle6 = new Obstacle("ob6", 11, 2285);
+    /**
+     * The Obstacle 7.
+     */
+    static Obstacle obstacle7 = new Obstacle("ob7", 13, 976);
+    /**
+     * The Obstacle 8.
+     */
+    static Obstacle obstacle8 = new Obstacle("ob8", 17, 0);
+    /**
+     * The Obstacle 9.
+     */
+    static Obstacle obstacle9 = new Obstacle("ob9", 8, 484);
+
+}
