29ff56792ff91ca50cf6a6fa1036f9d183b842f4
hurstie16s <samuel.hurst2@btinternet.com>
2023-05-01 14:40:54 +0100

    tests run just not with mvn test

--- src/test/java/comp2211/seg/ProcessDataModel/FileHandlerTest.java
+++ src/test/java/comp2211/seg/ProcessDataModel/FileHandlerTest.java
@@ -1,3 +1,5 @@
+package comp2211.seg.ProcessDataModel;
+
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
--- src/test/java/comp2211/seg/ProcessDataModel/RunwayTest.java
+++ src/test/java/comp2211/seg/ProcessDataModel/RunwayTest.java
@@ -1,5 +1,4 @@
-import comp2211.seg.ProcessDataModel.Obstacle;
-import comp2211.seg.ProcessDataModel.Runway;
+package comp2211.seg.ProcessDataModel;
 
 import javafx.beans.property.SimpleDoubleProperty;
 import org.apache.logging.log4j.LogManager;
@@ -8,11 +7,6 @@ import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
-
-import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
