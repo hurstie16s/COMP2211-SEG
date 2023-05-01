9f3837b3be69631f42545a21c2a7e0422f1df3c2
hurstie16s <samuel.hurst2@btinternet.com>
2023-05-01 10:40:47 +0100

    tests for designator calculations done

--- src/test/java/RunwayTest.java
+++ src/test/java/RunwayTest.java
@@ -188,7 +188,7 @@ public class RunwayTest {
      */
     @DisplayName("Re-designator test 09R/27L: form XXR/YYL where XX < YY")
     @Test
-    public void designatorTestXXRYYL() {
+    public void designatorXXRYYLTest() {
         String expectedOutput = "09R/27L";
         var runway = new Runway();
         runway.runwayDesignatorLeft.set("09R");
@@ -196,12 +196,83 @@ public class RunwayTest {
         assertEquals(expectedOutput, actualOutput, "Designator incorrectly calculated");
     }
 
-    @DisplayName("Re-designator test 09R/27L: form XXR/YYL where XX < YY")
+    @DisplayName("Re-designator test 09L/27R: form XXR/YYL where XX < YY")
     @Test
-    public void designatorTestXXLYYR() {
+    public void designatorXXLYYRTest() {
         String expectedOutput = "09L/27R";
         var runway = new Runway();
         runway.runwayDesignatorLeft.set("09L");
+        String actualOutput = runway.toString();
+        assertEquals(expectedOutput, actualOutput, "Designator incorrectly calculated");
+    }
+
+    @DisplayName("Re-designator test 09C/27C: form XXR/YYL where XX < YY")
+    @Test
+    public void designatorXXCYYCTest() {
+        String expectedOutput = "09C/27C";
+        var runway = new Runway();
+        runway.runwayDesignatorLeft.set("09C");
+        String actualOutput = runway.toString();
+        assertEquals(expectedOutput, actualOutput, "Designator incorrectly calculated");
+    }
+
+    @DisplayName("Re-designator test 09/27: form XXR/YYL where XX < YY")
+    @Test
+    public void designatorXXYYTest() {
+        String expectedOutput = "09/27";
+        var runway = new Runway();
+        runway.runwayDesignatorLeft.set("09");
+        String actualOutput = runway.toString();
+        assertEquals(expectedOutput, actualOutput, "Designator incorrectly calculated");
+    }
+
+    @DisplayName("Re-designator test 09R/27L: form XXR/YYL where XX > YY")
+    @Test
+    public void designatorXXRYYLFlipTest() {
+        String expectedOutput = "09R/27L";
+        var runway = new Runway();
+        runway.runwayDesignatorLeft.set("27L");
+        String actualOutput = runway.toString();
+        assertEquals(expectedOutput, actualOutput, "Designator incorrectly calculated");
+    }
+
+    @DisplayName("Re-designator test 09L/27R: form XXR/YYL where XX > YY")
+    @Test
+    public void designatorXXLYYRFlipTest() {
+        String expectedOutput = "09L/27R";
+        var runway = new Runway();
+        runway.runwayDesignatorLeft.set("27R");
+        String actualOutput = runway.toString();
+        assertEquals(expectedOutput, actualOutput, "Designator incorrectly calculated");
+    }
+
+    @DisplayName("Re-designator test 09C/27C: form XXR/YYL where XX > YY")
+    @Test
+    public void designatorXXCYYCFlipTest() {
+        String expectedOutput = "09C/27C";
+        var runway = new Runway();
+        runway.runwayDesignatorLeft.set("27C");
+        String actualOutput = runway.toString();
+        assertEquals(expectedOutput, actualOutput, "Designator incorrectly calculated");
+    }
+
+    @DisplayName("Re-designator test 09/27: form XXR/YYL where XX > YY")
+    @Test
+    public void designatorXXYYFlipTest() {
+        String expectedOutput = "09/27";
+        var runway = new Runway();
+        runway.runwayDesignatorLeft.set("27");
+        String actualOutput = runway.toString();
+        assertEquals(expectedOutput, actualOutput, "Designator incorrectly calculated");
+    }
+
+    @DisplayName("Re-designator test 09: form XX")
+    @Test
+    public void designatorXXTest() {
+        String expectedOutput = "09";
+        var runway = new Runway();
+        runway.runwayDesignatorLeft.set("09");
+        runway.dualDirectionRunway.set(false);
         String actualOutput = runway.toString();
         assertEquals(expectedOutput, actualOutput, "Designator incorrectly calculated");
     }
