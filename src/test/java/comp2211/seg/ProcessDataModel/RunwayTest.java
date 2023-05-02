bb948b47c577f8ec3b41153339ea18ee7ba3048d
hurstie16s <samuel.hurst2@btinternet.com>
2023-05-02 10:51:00 +0100

    error proofed tests, ensure pre-conditions to tests hold

--- src/test/java/comp2211/seg/ProcessDataModel/RunwayTest.java
+++ src/test/java/comp2211/seg/ProcessDataModel/RunwayTest.java
@@ -36,36 +36,40 @@ public class RunwayTest {
     public void designatorXXRYYLTest() {
         String expectedOutput = "09R/27L";
         var runway = new Runway();
+        runway.dualDirectionRunway.set(true);
         runway.runwayDesignatorLeft.set("09R");
         String actualOutput = runway.toString();
         assertEquals(expectedOutput, actualOutput, "Designator incorrectly calculated");
     }
 
-    @DisplayName("Re-designator test 09L/27R: form XXR/YYL where XX < YY")
+    @DisplayName("Re-designator test 09L/27R: form XXL/YYR where XX < YY")
     @Test
     public void designatorXXLYYRTest() {
         String expectedOutput = "09L/27R";
         var runway = new Runway();
+        runway.dualDirectionRunway.set(true);
         runway.runwayDesignatorLeft.set("09L");
         String actualOutput = runway.toString();
         assertEquals(expectedOutput, actualOutput, "Designator incorrectly calculated");
     }
 
-    @DisplayName("Re-designator test 09C/27C: form XXR/YYL where XX < YY")
+    @DisplayName("Re-designator test 09C/27C: form XXC/YYC where XX < YY")
     @Test
     public void designatorXXCYYCTest() {
         String expectedOutput = "09C/27C";
         var runway = new Runway();
+        runway.dualDirectionRunway.set(true);
         runway.runwayDesignatorLeft.set("09C");
         String actualOutput = runway.toString();
         assertEquals(expectedOutput, actualOutput, "Designator incorrectly calculated");
     }
 
-    @DisplayName("Re-designator test 09/27: form XXR/YYL where XX < YY")
+    @DisplayName("Re-designator test 09/27: form XX/YY where XX < YY")
     @Test
     public void designatorXXYYTest() {
         String expectedOutput = "09/27";
         var runway = new Runway();
+        runway.dualDirectionRunway.set(true);
         runway.runwayDesignatorLeft.set("09");
         String actualOutput = runway.toString();
         assertEquals(expectedOutput, actualOutput, "Designator incorrectly calculated");
@@ -76,36 +80,40 @@ public class RunwayTest {
     public void designatorXXRYYLFlipTest() {
         String expectedOutput = "09R/27L";
         var runway = new Runway();
+        runway.dualDirectionRunway.set(true);
         runway.runwayDesignatorLeft.set("27L");
         String actualOutput = runway.toString();
         assertEquals(expectedOutput, actualOutput, "Designator incorrectly calculated");
     }
 
-    @DisplayName("Re-designator test 09L/27R: form XXR/YYL where XX > YY")
+    @DisplayName("Re-designator test 09L/27R: form XXL/YYR where XX > YY")
     @Test
     public void designatorXXLYYRFlipTest() {
         String expectedOutput = "09L/27R";
         var runway = new Runway();
+        runway.dualDirectionRunway.set(true);
         runway.runwayDesignatorLeft.set("27R");
         String actualOutput = runway.toString();
         assertEquals(expectedOutput, actualOutput, "Designator incorrectly calculated");
     }
 
-    @DisplayName("Re-designator test 09C/27C: form XXR/YYL where XX > YY")
+    @DisplayName("Re-designator test 09C/27C: form XXC/YYC where XX > YY")
     @Test
     public void designatorXXCYYCFlipTest() {
         String expectedOutput = "09C/27C";
         var runway = new Runway();
+        runway.dualDirectionRunway.set(true);
         runway.runwayDesignatorLeft.set("27C");
         String actualOutput = runway.toString();
         assertEquals(expectedOutput, actualOutput, "Designator incorrectly calculated");
     }
 
-    @DisplayName("Re-designator test 09/27: form XXR/YYL where XX > YY")
+    @DisplayName("Re-designator test 09/27: form XX/YY where XX > YY")
     @Test
     public void designatorXXYYFlipTest() {
         String expectedOutput = "09/27";
         var runway = new Runway();
+        runway.dualDirectionRunway.set(true);
         runway.runwayDesignatorLeft.set("27");
         String actualOutput = runway.toString();
         assertEquals(expectedOutput, actualOutput, "Designator incorrectly calculated");
@@ -127,6 +135,7 @@ public class RunwayTest {
     public void designatorErrorCaseTest() {
         String expectedOutput = "ERROR/ERROR";
         var runway = new Runway();
+        runway.dualDirectionRunway.set(true);
         runway.runwayDesignatorLeft.set("09A");
         String actualOutput = runway.toString();
         assertEquals(expectedOutput, actualOutput, "Designator incorrectly calculated");
@@ -146,9 +155,9 @@ public class RunwayTest {
     R5 O9
      */
 
-    @DisplayName("Runway Recalculation test: Runway 2 (09L27R), Obstacle 1")
+    @DisplayName("Runway Recalculation test: Runway 2 (09L/27R), Obstacle 1, Land Towards")
     @Test
-    public void runwayRecalculationTestR2O1() {
+    public void runwayRecalculationR2O1LandTowardsTest() {
         var runway = RunwayTestData.runway2;
         var obstacle = RunwayTestData.obstacle1;
 
@@ -157,69 +166,106 @@ public class RunwayTest {
         // Land towards
         RunwayCalculations.calculateLandTowardLeft(runway);
         RunwayCalculations.calculateLandTowardRight(runway);
+
+        assertValues(
+                runway.getLeftLda(),
+                runway.getRightLda(),
+                0,
+                3345
+        );
+    }
+    @DisplayName("Runway Recalculation test: Runway 2 (09L/27R), Obstacle 1, Takeoff Towards")
+    @Test
+    public void runwayRecalculationR2O1TakeOffTowardsTest() {
+        var runway = RunwayTestData.runway2;
+        var obstacle = RunwayTestData.obstacle1;
+
+        runway.addObstacle(obstacle);
+        runway.runwayObstacle.distFromThresholdProperty().set(-50);
         // Takeoff towards
         RunwayCalculations.calculateTakeOffTowardLeft(runway);
         RunwayCalculations.calculateTakeOffTowardRight(runway);
 
         assertValues(
-                runway.getLeftLda(),
-                runway.getRightLda(),
                 runway.getLeftTora(),
                 runway.getRightTora(),
                 runway.getLeftToda(),
                 runway.getRightToda(),
                 runway.getLeftAsda(),
                 runway.getRightAsda(),
-                0,
-                3345,
                 0,
                 2985,
                 0,
                 2985,
                 0,
                 2985
-
         );
+    }
 
+    @DisplayName("Runway Recalculation test: Runway 2 (09L/27R), Obstacle 1, Land Over")
+    @Test
+    public void runwayRecalculationR2O1LandOverTest() {
+        var runway = RunwayTestData.runway2;
+        var obstacle = RunwayTestData.obstacle1;
+
+        runway.addObstacle(obstacle);
+        runway.runwayObstacle.distFromThresholdProperty().set(-50);
         // Land Over
         RunwayCalculations.calculateLandOverLeft(runway);
         RunwayCalculations.calculateLandOverRight(runway);
+
+        assertValues(
+                runway.getLeftLda(),
+                runway.getRightLda(),
+                2985,
+                0
+        );
+    }
+
+    @DisplayName("Runway Recalculation test: Runway 2 (09L/27R), Obstacle 1, Takeoff Away")
+    @Test
+    public void runwayRecalculationR2O1TakeoffAwayTest() {
+        var runway = RunwayTestData.runway2;
+        var obstacle = RunwayTestData.obstacle1;
+
+        runway.addObstacle(obstacle);
+        runway.runwayObstacle.distFromThresholdProperty().set(-50);
         // Takeoff away
         RunwayCalculations.calculateTakeOffAwayLeft(runway);
         RunwayCalculations.calculateTakeOffAwayRight(runway);
 
         assertValues(
-                runway.getLeftLda(),
-                runway.getRightLda(),
                 runway.getLeftTora(),
                 runway.getRightTora(),
                 runway.getLeftToda(),
                 runway.getRightToda(),
                 runway.getLeftAsda(),
                 runway.getRightAsda(),
-                2985,
-                0,
                 3095,
                 0,
                 3095,
                 78,
                 3095,
                 0
-
         );
     }
 
     private void assertValues(
             double leftLDA_actual,
             double rightLDA_actual,
+            double leftLDA_expected,
+            double rightLDA_expected
+    ) {
+        assertEquals(leftLDA_expected, leftLDA_actual, "Left LDA Incorrect");
+        assertEquals(rightLDA_expected , rightLDA_actual, "Right LDA Incorrect");
+    }
+    private void assertValues(
             double leftTORA_actual,
             double rightTORA_actual,
             double leftTODA_actual,
             double rightTODA_actual,
             double leftASDA_actual,
             double rightASDA_actual,
-            double leftLDA_expected,
-            double rightLDA_expected,
             double leftTORA_expected,
             double rightTORA_expected,
             double leftTODA_expected,
@@ -227,8 +273,6 @@ public class RunwayTest {
             double leftASDA_expected,
             double rightASDA_expected
     ) {
-        assertEquals(leftLDA_expected, leftLDA_actual, "Left LDA Incorrect");
-        assertEquals(rightLDA_expected , rightLDA_actual, "Right LDA Incorrect");
         assertEquals(leftTORA_expected, leftTORA_actual, "Left TORA Incorrect");
         assertEquals(rightTORA_expected, rightTORA_actual, "Right TORA Incorrect");
         assertEquals(leftTODA_expected, leftTODA_actual, "Left TODA Incorrect");
